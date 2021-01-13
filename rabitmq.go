13584625b359ad630c562f6f9384defacaf7fb0d
ARMIN <armingodarzi1380@gmail.com>
2021-01-13 21:26:45 +0330

    working on new feature ... , rabitmq added

--- rabitmq.go
+++ rabitmq.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-  "github.com/streadway/amqp"
+	"context"
+	"github.com/streadway/amqp"
 )
 
+var url = flag.String("url", "amqp://guest:guest@127.0.0.1:55005/", "Amqp url for both the publishe and subscriber")
+
 type Rabitmq struct {
-  
+	*amqp.Connection
+	*amqp.Channel
+}
+
+func NewRabitmq(ctx context.Context) (*rabitmq, error) {
+	conn, err := amqp.Dial(*url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return &rabitmq{
+		conn:    conn,
+		channel: ch,
+	}, nil
+}
+
+func (q *rabitmq) EnqueueMessage(ctx context.Context, targetId uint64, msg []byte) error {
+	_, err := q.channel.QueueDeclare(fmt.Sprintf("user_%d", targetId), true, false, false, true, nil)
+	if err != nil {
+		return err
+	}
+	return q.channel.Publish("", fmt.Sprintf("user_%d", targetId), true, false, false, amqp.Publishing{
+		Body: msg,
+	})
 }
